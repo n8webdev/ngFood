129f6bcc38fad827013708c54e13a5005b209ab8
unknown <Torquato, Nathan>
2017-09-24 01:11:09 -0300

    Configure Route Params and Service method for getting a RecipeDetail

--- src/app/recipe-book/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { ShoppingService } from './../../shopping-list/shopping.service';
 import { RecipeService } from './../recipe.service';
 import { Component, OnInit, Input } from '@angular/core';
@@ -10,11 +11,21 @@ import { Recipe } from '../Recipe';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input() recipe: Recipe;
+  id: number;
+  recipe: Recipe;
 
-  constructor(private _sl: ShoppingService) { }
+  constructor(
+    private _recipeS: RecipeService,
+    private route: ActivatedRoute,
+    private _sl: ShoppingService
+  ) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(params => {
+        this.id = +params['id'];
+        this.recipe = this._recipeS.getRecipe(this.id);
+      });
   }
 
   addToCart() {
--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -24,6 +24,10 @@ export class RecipeService {
   )
   ];
 
+  getRecipe(id: number): Recipe {
+    return this.recipes[id];
+  }
+
   getRecipes() {
     return this.recipes.slice();
   }
