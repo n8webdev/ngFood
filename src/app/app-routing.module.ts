461472e6aaf532d3b940941f34ebbda84b32cd61
unknown <Torquato, Nathan>
2017-10-15 17:50:15 -0200

    Clean up app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AuthGuardService } from './auth/auth-guard.service';
 
-import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { StartComponent } from './recipe-book/start/start.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
-import { RecipesComponent } from './recipe-book/recipes/recipes.component';
-import { SignupComponent } from './auth/signup/signup.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
