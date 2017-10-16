d6dbef1c85fce75a5d1237be77046984f5dd2d7c
unknown <Torquato, Nathan>
2017-10-16 01:54:30 -0200

    Preload Lazy loaded modules

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { AuthGuardService } from '../auth/auth-guard.service';
 
@@ -8,12 +8,12 @@ import { HomeComponent } from './home/home.component';
 const appRoutes: Routes = [
   // { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: '', component: HomeComponent },
-  { path: 'recipes', loadChildren: './recipe-book/recipes.module#RecipesModule' },
-  { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule', canLoad: [AuthGuardService] }
+  { path: 'recipes', loadChildren: '../recipe-book/recipes.module#RecipesModule' },
+  { path: 'shopping-list', loadChildren: '../shopping-list/shopping-list.module#ShoppingListModule', canLoad: [AuthGuardService] }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 
