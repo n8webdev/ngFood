91f96e94b870ad69852cd5cfc7e56a5a8feb18ad
unknown <Torquato, Nathan>
2017-10-16 01:31:52 -0200

    Load Providers on CoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,6 @@ import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 
-import { AuthService } from './auth/auth.service';
-import { AuthGuardService } from './auth/auth-guard.service';
-import { DataStorageService } from './shared/data-storage.service';
-import { RecipeService } from './recipe-book/recipe.service';
-import { ShoppingService } from './shopping-list/shopping.service';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -23,13 +17,8 @@ import { AppComponent } from './app.component';
     SharedModule,
     AuthModule
   ],
-  providers: [
-    ShoppingService,
-    RecipeService,
-    DataStorageService,
-    AuthService,
-    AuthGuardService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { SharedModule } from './../shared/shared.module';
 
+import { AuthGuardService } from './../auth/auth-guard.service';
+import { AuthService } from './../auth/auth.service';
+import { DataStorageService } from './../shared/data-storage.service';
+import { RecipeService } from './../recipe-book/recipe.service';
+import { ShoppingService } from './../shopping-list/shopping.service';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 
@@ -15,6 +21,13 @@ import { HomeComponent } from './home/home.component';
   imports: [
     SharedModule,
     AppRoutingModule
+  ],
+  providers: [
+    ShoppingService,
+    RecipeService,
+    DataStorageService,
+    AuthService,
+    AuthGuardService
   ],
   exports: [
     NavbarComponent,
