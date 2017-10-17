72d20d960f2706427eb5fc76194700d0c0180a8f
unknown <Torquato, Nathan>
2017-10-17 00:55:40 -0200

    HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CoreModule,
+    HttpClientModule,
     SharedModule,
     AuthModule
   ],
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,4 +1,4 @@
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -14,14 +14,14 @@ export class DataStorageService implements OnInit {
   constructor(
     private _auth: AuthService,
     private _recipes: RecipeService,
-    private http: Http
+    private http: HttpClient
   ) { }
 
   ngOnInit(): void {
     // this.token = this._auth.getToken();
   }
 
-  saveRecipes(): Observable<Response> {
+  saveRecipes() {
     const token = this._auth.getToken();
     return this.http.put(
       'https://lazy-test-cbd43.firebaseio.com/ngfood/recipes.json?auth=' + token,
@@ -32,11 +32,10 @@ export class DataStorageService implements OnInit {
   getRecipes(): void {
     const token = this._auth.getToken();
     // we don't need to return the data because we subscribe to it right here
-    this.http.get('https://lazy-test-cbd43.firebaseio.com/ngfood/recipes.json?auth=' + token)
+    this.http.get<Recipe[]>('https://lazy-test-cbd43.firebaseio.com/ngfood/recipes.json?auth=' + token)
       .map(
-        (response: Response) => {
+        (recipes) => {
           // make sure our response data has at least an empty array for Ingredients
-          const recipes = response.json();
           for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               console.log(recipe);
